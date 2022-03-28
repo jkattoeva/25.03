15ab33f478bc00f357b2dd8f5b24d4451433c909
Janetta Kataoeva <jkattoeva@gmail.com>
2022-03-28 12:48:37 +0600

    Create a List Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import List from "./component/List/List";
 function App() {
+  const veggies = ["Carrot", "Potato", "Strawberry"];
+  const fruits = [
+    { name: "Mango", color: "Yellow"},
+    { name: "Apple", color: "Green"},
+    { name: "Cherry", color: "Red"},
+  ];
+
+  const veggiesList = veggies.map((veggie) => {
+    return <li>{veggie}</li>
+  });
+    
+    
+  const fruitsList = fruits.map( (fruit) => (
+    <li>
+      {fruit.name}: {fruit.color}
+    </li>
+  ));
+
   return (
     <div className="App">
+      <h1>Hello world</h1>
+
+      <ul>
+      {veggiesList}
+      </ul>
+
+      <ul>
+        {fruitsList}
+      </ul>
+
+      <List src={veggies} />
     </div>
   );
 }
---
+++ src/component/List/List.js
@@ -0,0 +1,9 @@
+import "./List.css";
+
+function List(props) {
+  const list = props.src.map(item => <li>{item}</li>);
+
+  return <ul className="List">{list}</ul>;
+}
+
+export default List;
